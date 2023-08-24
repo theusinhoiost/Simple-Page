4959808fe4467881ee7af061c9e0bd3799831dea
Matheus Iost <137854934+theusinhoiost@users.noreply.github.com>
2023-08-24 02:38:29 -0300

    edit js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,10 @@
+let date = new Date();
+let year = date.getFullYear();
+
 const button = document.querySelector('#send');
-button.addEventListener('click', function(){
-alert(`oi`)
-
-
+button.addEventListener('click', function () {
+    const firstName = document.getElementById('first-name').value;
+    const surname = document.getElementById('surname').value;
+    const email = document.getElementById('email').value;
+    const msg = document.getElementById('msg').value;
 })
